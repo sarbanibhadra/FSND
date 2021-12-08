edb202d192c5d9666faceaf9cd79d66cd3b6abed
Sarbani Das <sarbani.bhadra@gmail.com>
2021-12-08 23:13:11 +0000

    projects/03_coffee_shop_full_stack

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -15,9 +15,9 @@ CORS(app)
 @TODO uncomment the following line to initialize the datbase
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
-!! Running this function will add one
+!! Running this funciton will add one
 '''
-# db_drop_and_create_all()
+db_drop_and_create_all()
 
 # ROUTES
 '''
@@ -28,6 +28,23 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks', methods=['GET'])
+def get_drinks():
+
+    all_drinks = Drink.query.all()
+    drinks = {}
+
+    if len(all_drinks) == 0:  
+        abort(404)
+   
+
+    for d in all_drinks:
+        drinks = [d.short()]
+
+    return jsonify({
+        "success": True, 
+        "drinks": drinks
+    }), 200
 
 
 '''
@@ -38,6 +55,24 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks-detail', methods=['GET'])
+@requires_auth('get:drinks-detail')
+def get_drinks_detail(payload):
+    print("drinks -details")
+    all_drinks = Drink.query.all()
+
+    drinks = {}
+
+    if all_drinks is None:  
+        abort(404)
+
+    for d in all_drinks:
+        drinks = [d.long()]
+
+    return jsonify({
+        'success': True,
+        'drinks': drinks
+    }), 200
 
 
 '''
@@ -49,7 +84,25 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
-
+app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
+def post_drinks(payload):
+    request_bar = request.get_json()
+    title = request_bar.get('title', None)
+    print(title)
+    recipe = request_bar.get('recipe', None)
+    print(recipe)
+    if request_bar is null:
+        abort(400)
+    try:
+        new_drink = Drink(title = title, recipe = json.dumps(recipe))
+        new_drink.title = title
+        new_drink.recipe =  json.dumps(recipe)
+        new_drink.insert()
+        new_drink = [new_drink.long()]
+        return jsonify({'success': True, 'drinks': new_drink}), 200
+    except:
+        return json.dumps({'success': False, 'error': "An error occurred" }), 500
 
 '''
 @TODO implement endpoint
@@ -62,6 +115,30 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<int:id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def update_the_drink_id(payload, id):
+    request_bar = request.get_json()
+    drink = Drink()
+    drink = Drink.query.filter(Drink.id == id).one_or_none()
+    if drink is None:
+        abort(404)
+
+    try:
+        print(request_bar)
+        if request_bar['title'] is not None:
+            drink.title = request_bar['title']
+        
+        if request_bar.get('recipe') is not None:
+            drink.recipe =  json.dumps([request_bar['recipe']])
+               
+        drink.update()
+        drink = [drink.long()]
+        return jsonify({'success': True, 'drinks': drink}), 200
+
+    except:
+        return json.dumps({'success': False, 'error': "An error occurred" }), 500 
+
 
 
 '''
@@ -74,9 +151,24 @@ CORS(app)
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<int:id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(payload, id):
+    print("inside delete")
+    drink = Drink.query.filter(Drink.id == id).one_or_none()
 
+    if drink is None:
+        abort(404)
 
-# Error Handling
+    try:
+        drink.delete()
+        return jsonify({'success': True, 'delete': id}), 200
+
+    except:
+        return json.dumps({'success': False, 'error': "An error occurred" }), 500
+
+    
+
 '''
 Example error handling for unprocessable entity
 '''
@@ -106,13 +198,49 @@ def unprocessable(error):
 @TODO implement error handler for 404
     error handler should conform to general task above
 '''
+@app.errorhandler(404)
+def resource_not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "resource not found"
+    }), 404
 
 
 '''
 @TODO implement error handler for AuthError
     error handler should conform to general task above
 '''
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": 'Bad Request'
+    }), 400
 
-if __name__ == "__main__":
-    app.debug = True
-    app.run()
+
+@app.errorhandler(405)
+def method_not_allowed(error):
+    print(error)
+    return jsonify({
+        "success": False,
+        "error": 405,
+        "message": 'Method Not Allowed'
+    }), 405
+
+@app.errorhandler(403)
+def permission_not_present(error):
+    return jsonify({
+        "success": False,
+        "error": 403,
+        "message": 'Permission Not Present'
+    }), 403    
+
+@app.errorhandler(500)
+def error_present(error):
+    return jsonify({
+        "success": False,
+        "error": 500,
+        "message": 'An error occurred'
+    }), 500
--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  apiServerUrl: 'http://127.0.0.5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'test-coffee-shop.us', // the auth0 domain prefix
+    audience: 'coffee', // the audience set for the auth0 app
+    clientId: 'nFRAuDtOJ9SeuGbXoG4459d3Zxdu5iLG', // the client id generated for the auth0 app
+    callbackURL: 'http://127.0.0.1:8100', // the base url of the running ionic application. 
   }
 };
